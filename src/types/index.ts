cdde4010d29081c1a006ada7c261c5b6542e1da4
JuanVimon3 <jvillamonsalve@gmail.com>
2025-07-23 20:29:10 -0500

    add guitarla.ts project

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type Guitar = {
+    id: number;
+    name: string;
+    image: string;
+    description: string;
+    price: number;
+};
+
+// export interface CartItem extends Guitar  {
+//     quantity: number;
+// }
+export type CartItem = Guitar & {
+    quantity: number;
+}
